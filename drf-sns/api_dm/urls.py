53c23360ffad670962a3b401396a64a51ba3b984
Tsubasa Takahashi <2bsttas@gmail.com>
2020-08-13 11:32:08 +0900

    set both api_dm's and api_user's urls

--- drf-sns/api_dm/urls.py
+++ drf-sns/api_dm/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_dm import views
 
 
 router = DefaultRouter()
+router.register('message', views.MessageViewSet, basename='message')
+router.register('inbox', views.InboxListView, basename='inbox')
 
 app_name = 'dm'
 
--- drf-sns/api_user/urls.py
+++ drf-sns/api_user/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from api_user import views
 
 
 router = DefaultRouter()
+router = register('approval', views.FriendRequestViewSet)
+router = register('profile', views.ProfileViewSet)
 
 app_name = 'user'
 
 urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+    path('myprofile/', views.MyProfileListView.as_view(), name='myprofile'),
+    path('token/', views.CreateTokenView.as_view(), name='token'), 
     path('', include(router.urls)),
 ]
